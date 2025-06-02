1972091b9e578d74491e6ae8907ec04b268b9014
chemPolonium <chempolonium@outlook.com>
2025-06-02 17:06:30 +0800

    some modification

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # roxy-equation
 Convert Racket expression to LaTeX equations
+
+Example:
+```
+(displayln (latex '(+ 1 2 3)))
+
+(displayln (latex `((+ 1 2 3))))
+
+(displayln (latex `(,(+ 1 2 3) "Hello!")))
+
+(displayln (latex '(paren (+ 1 2 3))))
+
+(displayln (latex '(matrix (1 2 3) (4 5 6) (7 8 9))))
+
+(displayln (latex '(matrix (1 (+ 2 2 2) 3) (4 5 6) (7 8 9))))
+
+(displayln (latex '(1 + 2 * 3)))
+```
+
+Output:
+```
+1 + 2 + 3
+
+1 + 2 + 3
+
+6 Hello!
+
+\left( 1 + 2 + 3 \right)
+
+\begin{matrix}
+1 & 2 & 3 \\
+4 & 5 & 6 \\
+7 & 8 & 9
+\end{matrix}
+
+\begin{matrix}
+1 & 2 + 2 + 2 & 3 \\
+4 & 5 & 6 \\
+7 & 8 & 9
+\end{matrix}
+
+1 + 2 * 3
+```
--- roxy-equation.rkt
+++ roxy-equation.rkt
@@ -1,130 +1,280 @@
 #lang racket
 
-(define (sexp->latex sexp)
-  (apply string-append (flatten (tex-preprocess sexp))))
+; 洛琪希：一个简单的 S-表达式到 LaTeX 的转换器
+; 这个转换器支持基本的数学符号、希腊字母、矩阵、数组等。
+; 它使用 Racket 的数据结构和函数来处理 S-表达式，并生成相应的 LaTeX 代码。
 
-(define (tex-preprocess sexp)
-  (match sexp
-    [(? string? sexp) sexp]
-    [(list-rest 'sup res) (tex-sup res)]
-    [(list-rest 'sub res) (tex-sub res)]
-    [(list-rest 'frac res) (tex-frac res)]
-    [(list-rest 'bmat res) (tex-bmatrix res)]
-    [(list-rest 'sum res) (tex-sum res)]
-    [(list-rest 'brak res) (tex-bracket res)]
-    [(list-rest 'sbrk res) (tex-squarebracket res)]
-    [(list-rest 'brac res) (tex-brace res)]
-    [(list-rest 'arr res) (tex-array res)]
-    [(list-rest 'lbrc res) (tex-left-brace res)]
-    [(? pair? sexp) (map tex-preprocess sexp)]
-    ['coma ","]
-    [(? greek? sexp) (list "\\" (symbol->string sexp) " ")]
-    [(? tex-sym-assoc sexp) (list "\\" (cdr (tex-sym-assoc sexp)) " ")]
-    [(? symbol? sexp) (symbol->string sexp)]
-    [(? number? sexp) (number->string sexp)]
-    [_ (error "no matched")]))
+; (define (alatex sexp)
+;   (apply string-append (flatten (latex sexp))))
 
-(define (greek? x)
-  (memq x (list 'alpha 'beta 'gamma 'delta 'epsilon 'zeta 'eta 'theta
-                'iota 'kappa 'lambda 'mu 'nu 'xi 'omicron 'pi
-                'rho 'sigma 'tau 'upsilon 'phi 'chi 'psi 'omega
-                'Alpha 'Beta 'Gamma 'Delta 'Epsilon 'Zeta 'Eta 'Theta
-                'Iota 'Kappa 'Lambda 'Mu 'Nu 'Xi 'Omicron 'Pi
-                'Rho 'Sigma 'Tar 'Upsilon 'Phi 'Chi 'Psi 'Omega)))
+(define (latex sexp)
+  (if (pair? sexp)
+      (string-join
+       (cond
+         [(tex-binary? (car sexp))
+          (tex-binary (car sexp) (map latex (cdr sexp)))]
+         [(tex-paren? (car sexp))
+          (tex-paren (car sexp) (latex (cadr sexp)))]
+         [(tex-func1? (car sexp))
+          (tex-func1 (car sexp) (latex (cadr sexp)))]
+         [(tex-func2? (car sexp))
+          (tex-func2 (car sexp) (latex (cadr sexp)) (latex (caddr sexp)))]
+         [(tex-matrice? (car sexp))
+          (tex-matrice (car sexp) (map (lambda (r) (map latex r)) (cdr sexp)))]
+         [(eq? (car sexp) 'sum) (apply tex-sum (map latex (cdr sexp)))]
+         [else (list (string-join (map latex sexp)))])
+       "")
+      (cond
+        [(string? sexp) sexp]
+        [(number? sexp) (number->string sexp)]
+        [(tex-greek? sexp) (tex-greek sexp)]
+        [(tex-symbol? sexp) (tex-symbol sexp)]
+        [(symbol? sexp) (symbol->string sexp)])))
 
-(define (tex-sym-assoc x)
-  (assq x '((prod "times")
-            (div "div")
-            (pm "pm")
-            (mp "mp")
-            (tril "triangleleft")
-            (trir "triangleright")
-            (cdot "cdot")
-            (setm "setminus")
-            (star "star")
-            (ast "ast")
-            (cup "cup")
-            (cap "cap")
-            (scup "sqcup")
-            (scap "sqcap")
-            (vee "vee")
-            (wedg "wedge")
-            (circ "circ")
-            (bllt "bullet")
-            (oadd "oplus")
-            (osub "ominus")
-            (odot "odot")
-            (osls "oslash"))))
+;;; Greek Alphabet
 
-(define (tex-sup sexp)
-  (list "^{" (tex-preprocess sexp) "}"))
+(define tex-greek-hash
+  (hasheq
+   'alpha    "\\alpha"
+   'beta     "\\beta"
+   'gamma    "\\gamma"
+   'delta    "\\delta"
+   'epsilon  "\\epsilon"
+   'zeta     "\\zeta"
+   'eta      "\\eta"
+   'theta    "\\theta"
+   'iota     "\\iota"
+   'kappa    "\\kappa"
+   'lambda   "\\lambda"
+   'mu       "\\mu"
+   'nu       "\\nu"
+   'xi       "\\xi"
+   'omicron  "\\omicron"
+   'pi       "\\pi"
+   'rho      "\\rho"
+   'sigma    "\\sigma"
+   'tau      "\\tau"
+   'upsilon  "\\upsilon"
+   'phi      "\\phi"
+   'chi      "\\chi"
+   'psi      "\\psi"
+   'omega    "\\omega"
+   'Alpha    "\\Alpha"
+   'Beta     "\\Beta"
+   'Gamma    "\\Gamma"
+   'Delta    "\\Delta"
+   'Epsilon  "\\Epsilon"
+   'Zeta     "\\Zeta"
+   'Eta      "\\Eta"
+   'Theta    "\\Theta"
+   'Iota     "\\Iota"
+   'Kappa    "\\Kappa"
+   'Lambda   "\\Lambda"
+   'Mu       "\\Mu"
+   'Nu       "\\Nu"
+   'Xi       "\\Xi"
+   'Omicron  "\\Omicron"
+   'Pi       "\\Pi"
+   'Rho      "\\Rho"
+   'Sigma    "\\Sigma"
+   'Tau      "\\Tau"
+   'Upsilon  "\\Upsilon"
+   'Phi      "\\Phi"
+   'Chi      "\\Chi"
+   'Psi      "\\Psi"
+   'Omega    "\\Omega"))
 
-(define (tex-sub sexp)
-  (list "_{" (tex-preprocess sexp) "}"))
+(define (tex-greek? sexp)
+  (hash-has-key? tex-greek-hash sexp))
 
-(define (tex-frac sexp)
-  (if (not (= 2 (length sexp)))
-      (error "FRAC: invalid input")
-      (list "\\frac{" (tex-preprocess (first sexp)) "}{" (tex-preprocess (second sexp)) "}")))
+(define (tex-greek sexp)
+  (hash-ref tex-greek-hash sexp))
 
-(define (tex-array-content sexp)
-  (add-between (map (lambda (s)
-                      ; s is each line
-                      ; (map parse s) is to parse every single element
-                      (add-between (map tex-preprocess s) " & "))
-                    sexp)
-               " \\\\\n"))
+;;; Set and Logic Notations
 
-(define (tex-bmatrix sexp)
+(define tex-symbol-hash
+  (hasheq
+   'exists        "\\exists"
+   'nexists       "\\nexists"
+   'forall        "\\forall"
+   'in            "\\in"
+   'notin         "\\notin"
+   'ni            "\\ni"
+   'rightarrow    "\\rightarrow"
+   'to            "\\to"
+   'leftarrow     "\\leftarrow"
+   'gets          "\\gets"
+   'greater-than  "\\gt"
+   'less-than     "\\lt"
+   ; some functions are also treated as symbols
+   'sin           "\\sin"
+   'cos           "\\cos"
+   'tan           "\\tan"
+   'sec           "\\sec"
+   'csc           "\\csc"
+   'cot           "\\cot"
+   'arcsin        "\\arcsin"
+   'arccos        "\\arccos"
+   'arctan        "\\arctan"
+   'ln            "\\ln"))
+
+(define (tex-symbol? sexp)
+  (hash-has-key? tex-symbol-hash sexp))
+
+(define (tex-symbol sexp)
+  (hash-ref tex-symbol-hash sexp))
+
+;;; Functions with one argument
+; These functions are used for operations like square root, etc.
+
+(define tex-func1-hash
+  (hasheq
+   'dot   '("\\dot{"        "}")
+   'bar   '("\\bar{"        "}")
+   'hat   '("\\hat{"        "}")
+   'breve '("\\breve{"      "}")
+   'sqrt  '("\\sqrt{"       "}")
+   'overline    '("\\overline{"   "}")
+   'underline   '("\\underline{"  "}")
+   'overbrace   '("\\overbrace{"  "}")
+   'underbrace  '("\\underbrace{" "}")
+   'wideparen   '("\\wideparen{"  "}")
+   'underparen  '("\\underparen{"  "}")
+   'overrightarrow       '("\\overrightarrow{"      "}")
+   'overleftarrow        '("\\overleftarrow{"       "}")
+   'overleftrightarrow   '("\\overleftrightarrow{"  "}")
+   'underrightarrow      '("\\underrightarrow{"     "}")
+   'underleftarrow       '("\\underleftarrow{"      "}")
+   'underleftrightarrow  '("\\underleftrightarrow{" "}")
+   'overrightharpoon   '("\\overrightharpoon{"  "}")
+   'overleftharpoon    '("\\overleftharpoon{"   "}")
+   'underrightharpoon  '("\\underrightharpoon{" "}")
+   'underleftharpoon   '("\\underleftharpoon{"  "}")
+   ))
+
+(define (tex-func1? op)
+  (hash-has-key? tex-func1-hash op))
+
+(define (tex-func1 op sexp)
+  (match-let ([(list op1 op2) (hash-ref tex-func1-hash op)])
+    (list op1 sexp op2)))
+
+;;; Functions with two arguments
+; These functions are used for operations like fractions, superscripts, etc.
+
+(define tex-func2-hash
+  (hasheq
+   'sub       '("{"           "}_{"  "}")
+   'sup       '("{"           "}^{ " "}")
+   'frac      '("\\frac{"     "}{"   "}")
+   'overset   '("\\overset{"  "}{"   "}")
+   'underset  '("\\underset{" "}{"   "}")))
+
+(define (tex-func2? op)
+  (hash-has-key? tex-func2-hash op))
+
+(define (tex-func2 op sexp1 sexp2)
+  (match-let ([(list op1 op2 op3) (hash-ref tex-func2-hash op)])
+    (list op1 sexp1 op2 sexp2 op3)))
+
+;;; Functions with three arguments
+; These functions are used for large operators like integrals, sums, etc.
+
+(define tex-func3-hash
+  (hasheq
+   'int       '("\\int_{"  "}^{"    "}{" "}")
+   'oint      '("\\oint_{" "}^{"    "}{" "}")
+   'lim       '("\\lim_{"  " \\to " "}{" "}")))
+
+;;; Matrices and Arrays
+; These functions are used to format matrices and arrays in LaTeX.
+
+(define (tex-matrice-content sexp)
   (if (not (apply = (map length sexp)))
-      (error "BMATRIX: invalid input")
-      (list "\\begin{bmatrix}\n"
-            (tex-array-content sexp)
-            "\n\\end{bmatrix}")))
+      (error "MATRICE: invalid input" sexp)
+      ; add-between is used to join each line with " & "
+      ; and then join all lines with " \\\\ "
+      (string-join (map (lambda (r) ; r is each row
+                          (string-join r " & "))
+                        sexp)
+                   " \\\\\n")))
 
-(define (tex-array sexp)
-  (if (not (apply = (map length (rest sexp))))
-      (error "ARRAY: invalid input")
-      (list "\\begin{array}{"
-            (first sexp)
-            "}\n"
-            (tex-array-content (rest sexp))
-            "\n\\end{array} ")))
+(define tex-matrice-hash
+  (hasheq
+   'array    "array"
+   'matrix   "matrix"
+   'pmatrix  "pmatrix"
+   'bmatrix  "bmatrix"
+   'Bmatrix  "Bmatrix"
+   'vmatrix  "vmatrix"
+   'Vmatrix  "Vmatrix"))
 
-(define (tex-sum sexp)
-  (define n (length sexp))
-  (cond [(= n 3)
-         (list "\\sum_{" (tex-preprocess (first sexp)) "}^{"
-               (tex-preprocess (second sexp)) "}" (tex-preprocess (third sexp)))]
-        [(= n 1)
-         (list "{\\sum}" (tex-preprocess first sexp))]
-        [(= n 2)
-         (list "\\sum_{" (tex-preprocess (first sexp)) "}" (tex-preprocess (second sexp)))]
-        [else
-         (error "SUM: invalid input")]))
+(define (tex-matrice? op)
+  (hash-has-key? tex-matrice-hash op))
 
-(define (tex-bracket sexp)
-  (list "\\left( " (tex-preprocess sexp) "\\right) "))
+(define (tex-matrice op sexp)
+  (define op-string (hash-ref tex-matrice-hash op))
+  (list "\\begin{" op-string "}\n"
+        (tex-matrice-content sexp)
+        "\n\\end{" op-string "}"))
 
-(define (tex-squarebracket sexp)
-  (list "\\left[ " (tex-preprocess sexp) "\\right] "))
+(define (tex-sum . sexps)
+  (match sexps
+    [(list start end content) (list "\\sum_{" start "}^{" end "} " content)]
+    [(list range content) (list "\\sum_{" range "} " content)]
+    [(list content) (list "\\sum " content)]))
 
-(define (tex-brace sexp)
-  (list "\\left\\{ " (tex-preprocess sexp) "\\right\\} "))
+;;; Parentheses and Brackets
+; These functions are used to format parentheses, brackets, and other delimiters in LaTeX.
+; They ensure that the delimiters are properly sized according to the content inside them.
+; The delimiters can be parentheses, brackets, braces, angle brackets, vertical bars, etc.
 
-(define (tex-left-brace sexp)
-  (list "\\left\\{ " (tex-preprocess sexp) "\\right."))
+(define tex-paren-hash
+  (hasheq
+   'paren    '("\\left("         "\\right)")
+   'bracket  '("\\left["         "\\right]")
+   'brace    '("\\left\\{"       "\\right\\}")
+   'lbrace   '("\\left\\{"       "\\right.")
+   'angle    '("\\left\\langle"  "\\right\\rangle")
+   'vert     '("\\left|"         "\\right|")
+   'norm     '("\\left\\|"       "\\right\\|")
+   'floor    '("\\lfloor"        "\\rfloor")
+   'ceil     '("\\lceil"         "\\rceil")))
 
-(displayln (sexp->latex `("a" + "b" - alpha (sup "c"))))
-(displayln (sexp->latex `("a" "b" (frac ("a" "b") ("c" "d")))))
-(define abc `(a + b - c))
-(displayln (sexp->latex `(a + b - (c) + ,abc + "a" - (frac 3 gamma))))
-(displayln (sexp->latex `(a + b + (bmat (1 2 3) (4 5 6)))))
-(displayln (sexp->latex `(a + b + (sum (i = 1) (6) i))))
-(displayln (sexp->latex `(a prod b oadd c)))
-(displayln (sexp->latex `(a + (brak b + c) - (sbrk (d - e)) prod (brac f))))
-(displayln (sexp->latex `(lbrc (arr "ccc"
-                                    ((a + b) = 1)
-                                    (b coma (a > 3))
-                                    (c coma "otherwise")
-                                    (d = 5)))))
+(define (tex-paren? op)
+  (hash-has-key? tex-paren-hash op))
+
+(define (get-tex-paren op)
+  (hash-ref tex-paren-hash op))
+
+(define (tex-paren paren sexp)
+  (match-let ([(list left right) (get-tex-paren paren)])
+    (list left " " sexp " " right)))
+
+;;; Binary Operations which can be combined with other operations
+; These operations include addition, subtraction, multiplication, division, logical operations, etc.
+
+(define tex-binary-hash
+  (hasheq
+   '+    "+"
+   '-    "-"
+   '*    "\\,"
+   '/    "/"
+   'and  "\\land"
+   'or   "\\lor"
+   'xor  "\\oplus"))
+
+(define (tex-binary? op)
+  (hash-has-key? tex-binary-hash op))
+
+(define (tex-binary op sexps)
+  (define op-string (hash-ref tex-binary-hash op))
+  (add-between sexps (string-append " " op-string " ")))
+
+(displayln (latex '(+ 1 2 3)))
+(displayln (latex `((+ 1 2 3))))
+(displayln (latex `(,(+ 1 2 3) "Hello!")))
+(displayln (latex '(paren (+ 1 2 3))))
+(displayln (latex '(matrix (1 2 3) (4 5 6) (7 8 9))))
+(displayln (latex '(matrix (1 (+ 2 2 2) 3) (4 5 6) (7 8 9))))
+(displayln (latex '(1 + 2 * 3)))
